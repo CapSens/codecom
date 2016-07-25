42025cacd6035f86aeedf350ca7dbaa5740b2360
Yassine Zenati <yassine@capsens.eu>
2016-07-25 09:44:29 +0200

    no-commit-message

--- lib/capsens/codecom.rb
+++ lib/capsens/codecom.rb
@@ -8,17 +8,7 @@ module Capsens
     class Runner
       attr_accessor :previous_comment_index
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   initialize
-      #   #=> @return Expected returned value
-      #
-      # @return [Class] Describe what the method should return.
+
       def initialize
         self.previous_comment_index = -1
 
@@ -47,38 +37,13 @@ module Capsens
         end
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   template_options(line)
-      #   #=> @return Expected returned value
-      #
-      # @param line [Class] Write param definition here.
-      # @return [Class] Describe what the method should return.
-      def template_options(line)
         {
-          author_name: extract_author_name.strip,
+          author_name: extract_author_name,
           method_name: extract_method_name(line),
           params_name: extract_method_arguments(line)
         }
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   extract_method_arguments(line)
-      #   #=> @return Expected returned value
-      #
-      # @param line [Class] Write param definition here.
-      # @return [Class] Describe what the method should return.
       def extract_method_arguments(line)
         method = extract_method_name(line)
         if method.include?('(')
@@ -88,65 +53,22 @@ module Capsens
         end
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   extract_author_name
-      #   #=> @return Expected returned value
-      #
-      # @return [Class] Describe what the method should return.
       def extract_author_name
-        `git config user.name`
+        `git config user.name`.strip
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   extract_method_name(line)
-      #   #=> @return Expected returned value
-      #
-      # @param line [Class] Write param definition here.
-      # @return [Class] Describe what the method should return.
+      def extract_git_revision
+        `git --git-dir #{File.expand_path('../..', File.dirname(__FILE__))}/.git rev-parse --short HEAD`.strip
+      end
+
       def extract_method_name(line)
         line.strip.split('def ')[1]
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   black_listed_methods
-      #   #=> @return Expected returned value
-      #
-      # @return [Class] Describe what the method should return.
       def black_listed_methods
         [ :initialize, :permitted_params ]
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   process_line(line, index)
-      #   #=> @return Expected returned value
-      #
-      # @param line [Class] Write param definition here.
-      # @param index [Class] Write param definition here.
-      # @return [Class] Describe what the method should return.
       def process_line(line, index)
         if previous_comment_index == (index - 1)
           self.previous_comment_index = -1
@@ -156,19 +78,6 @@ module Capsens
         end
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   replace_template(data, options = {})
-      #   #=> @return Expected returned value
-      #
-      # @param data [Class] Write param definition here.
-      # @param options = {} [Class] Write param definition here.
-      # @return [Class] Describe what the method should return.
       def replace_template(data, options = {})
         data = data.gsub('%{method_name}', options.fetch(:method_name))
         data = data.gsub('%{author_name}', options.fetch(:author_name))
@@ -181,53 +90,21 @@ module Capsens
         data.gsub('# %{params}', params)
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   param_template
-      #   #=> @return Expected returned value
-      #
-      # @return [Class] Describe what the method should return.
       def param_template
         "# @param %{param} [Class] Write param definition here."
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   indent_template(template, index)
-      #   #=> @return Expected returned value
-      #
-      # @param template [Class] Write param definition here.
-      # @param index [Class] Write param definition here.
-      # @return [Class] Describe what the method should return.
       def indent_template(template, index)
         template.strip.split("\n").map(&:strip).map do |slice|
           slice.prepend(' ' * index)
         end.join("\n")
       end
 
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   template
-      #   #=> @return Expected returned value
-      #
-      # @return [Class] Describe what the method should return.
       def template
         "
+        # @engine capsens-codecom
+        # @commit #{extract_git_revision}
+        #
         # Describe here what the method should be used for.
         # Remember to add use case examples if possible.
         #
