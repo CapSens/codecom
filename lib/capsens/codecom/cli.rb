ccd514d0174a7989b02615a3e0ddc9e90cb1f89c
Yassine Zenati <yassine@capsens.eu>
2016-07-25 14:52:10 +0200

    no-commit-message

--- lib/capsens/codecom/cli.rb
+++ lib/capsens/codecom/cli.rb
@@ -4,27 +4,9 @@ module Capsens
   module Codecom
     class CLI < Thor
       default_task :start
-
       desc :start, "starts engine to add missing comments"
       option :force
 
-
-
-
-
-
-
-      # Describe here what the method should be used for.
-      # Remember to add use case examples if possible.
-      #
-      # @author Yassine Zenati
-      #
-      # Examples:
-      #
-      #   start
-      #   #=> @return Expected returned value
-      #
-      # @return [Class] Describe what the method should return.
       def start
         Capsens::Codecom::Runner.new(options[:force])
       end
